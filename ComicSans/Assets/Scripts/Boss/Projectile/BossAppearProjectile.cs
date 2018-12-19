2e9e73ce97bb8ef27cfea7e445d0421d325987a9
Luís Eduardo Rozante de Freitas Pereira <luis.eduardo.rozante@gmail.com>
2018-12-19 18:38:36 -0200

    Additions to the first boss and improvements
    - Added teletransport attack to the Cueio boss.
    - Started adding a 2nd phase to the boss (WIP).
    - Removed some unused Unity tags and packages.

---
+++ ComicSans/Assets/Scripts/Boss/Projectile/BossAppearProjectile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Scripts/Boss/Appear Projectile")]
+public class BossAppearProjectile : MonoBehaviour {
+
+	[SerializeField] private float appearDelay;
+	[SerializeField] private float disappearDelay;
+	[SerializeField] private Collider2D _collider;
+
+	private float timer;
+
+	private void Awake()
+	{
+
+		_collider.enabled = false;
+		timer = 0;
+
+	}
+
+	void Update ()
+	{
+		
+		if(!_collider.enabled)
+		{
+			timer += Time.deltaTime;
+			if(timer >= appearDelay)
+			{
+				_collider.enabled = true;
+				timer = 0;
+			}
+		}
+		else
+		{
+			timer += Time.deltaTime;
+			if(timer >= disappearDelay)
+				Destroy(gameObject);
+			
+		}
+	}
+}
---
+++ ComicSans/Assets/Scripts/Boss/Projectile/BossProjectileSpawner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Scripts/Boss/Projectile Spawner")]
+public class BossProjectileSpawner : MonoBehaviour {
+
+	[SerializeField] private int numberOfAttack;
+	[SerializeField] private float delay;
+
+	private int currentAttack;
+	private float timer;
+
+	[SerializeField] GameObject projectile;
+
+	enum SpawnType { SinglePosition, MultiplePosition, PlayerPosition }
+	[SerializeField] private SpawnType spawnType;
+	[SerializeField] private Vector3[] spawnPositions;
+
+	private void Awake () 
+	{
+
+		currentAttack = 0;
+		timer = delay + 1;
+
+	}
+
+	private void Update () {
+		
+		if(currentAttack < numberOfAttack) 
+		{
+			if(timer >= delay) 
+			{
+				
+				Attack();
+
+				currentAttack++;
+				timer = 0;
+			}
+
+			timer += Time.deltaTime;
+
+		} 
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	private void Attack() {
+		
+		if(spawnType == SpawnType.SinglePosition) 
+		{
+			Instantiate(projectile, spawnPositions[0], new Quaternion());
+		} 
+		else if(spawnType == SpawnType.MultiplePosition) 
+		{
+			Instantiate(projectile, spawnPositions[currentAttack], new Quaternion());
+		}
+		else
+		{
+			GameObject _player = GameObject.FindGameObjectWithTag("Player");
+			if(_player != null)
+				Instantiate(projectile, _player.transform.position, new Quaternion());
+			else
+				Debug.LogWarning("(ProjectileSpawner) Player not found!");
+		}
+
+	}
+}
