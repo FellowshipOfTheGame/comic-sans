b853d8d94298cbb8491a53101e8279f108abee0a
Luís Eduardo Rozante <luis.eduardo.rozante@gmail.com>
2020-08-28 11:30:13 -0300

    Configured WebGL, reselecting UI if player clicks out

---
+++ ComicSans/Assets/Scripts/UI/HideInWebGL.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HideInWebGL : MonoBehaviour
+{
+    
+    public GameObject target;
+
+    void Start()
+    {
+
+        #if UNITY_WEBGL
+
+            target.SetActive(false);
+
+        #endif
+
+    }
+}
---
+++ ComicSans/Assets/Scripts/UI/ReselectUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+// Used to reselect UI in case the keyboard selection is lost.
+public class ReselectUI : MonoBehaviour
+{
+
+    EventSystem eventSystem;
+
+    public GameObject target;
+
+    void Awake() {
+
+        eventSystem = FindObjectOfType<EventSystem>();
+
+    }
+
+    void Update()
+    {
+
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)) {
+
+            eventSystem.SetSelectedGameObject(target);
+
+        }
+
+    }
+}
